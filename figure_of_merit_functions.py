7c24299cf1d2ad122ba39f4e76e2e0fd3a323a65
Mark <mathis.a.mark@gmail.com>
2017-03-23 15:13:22 -0400

    Commit from lab computer

--- figure_of_merit_functions.py
+++
@@ -1,7 +0,0 @@
-"""These functions calculate the figure of merit for each person"""
-def test_genes(genes):
-    """Compute figure of merit that is least squares of y = index_value"""
-    figure_of_merit = 0
-    for i in range(genes.size):
-        figure_of_merit = figure_of_merit + (genes[i]-i)*(genes[i]-i)
-    return -figure_of_merit
